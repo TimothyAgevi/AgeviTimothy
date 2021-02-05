4d4b71d3ec6ae24f4e4c3d15a8690aa83bc198aa
TimothyAgevi <timothyagevi@gmail.com>
2021-02-05 20:12:40 +0300

    solved bus_fare_challenge

--- week_1/bus_fare_challenge.py
+++ week_1/bus_fare_challenge.py
@@ -6,13 +6,13 @@ day=date.strftime("%A")[0:3]
 mon_fri=["Mon","Tue","Wed","Thu","Fri"]
 
 def get_fare(day):
-    if day in mon_fri:
-      return 100
-    elif day =="Sat":
-        return 60
-    else:
-        return 80
+  if day in mon_fri:
+    return 100
+  elif day == "Sat":
+    return 60
+  return 80
 
-        fare=get_fare(day)
 
-        print(f"""Date:{date}/n Day:{day}/n Fare:{fare}""")    
+fare=get_fare(day)
+
+print(f"""Date:{date}\nDay:{day}\nFare:{fare}""")    
