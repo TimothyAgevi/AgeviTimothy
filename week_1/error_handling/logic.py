0f23866787182ba39ca716f9f487e01efbb17d6d
TimothyAgevi <timothyagevi@gmail.com>
2021-02-10 23:14:20 +0300

    updated syntax.py file by completing the else statement

--- week_1/error_handling/logic.py
+++ week_1/error_handling/logic.py
@@ -1,6 +1,6 @@
 x = 45
 y = 206
-if x < y:
+if x > y:
     print(str(x) + ' is greater than ' + str(y))
 else:
     print('all cool')
--- week_1/error_handling/syntax.py
+++ week_1/error_handling/syntax.py
@@ -2,3 +2,6 @@ x = 42
 y = 206
 if x == y:
     print("Success")
+else:
+    print("eror")
+    
