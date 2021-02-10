e957f01cd75372247862f8cd4993593a0bbacb26
TimothyAgevi <timothyagevi@gmail.com>
2021-02-10 11:19:39 +0300

    Added Print file

---
+++ week_1/print/input.py
@@ -0,0 +1,12 @@
+# The input function allows you to prompt the user to type something
+
+# You need to declare a variable to hold the value entered by the user
+# IMPORTANT: When you declare a variable, it has to start with small letter
+name = input('What is your name? ')
+
+print('Great, your name is ' + name)
+
+# The user can type their name on the next line with the \n command / not on the same line as above
+name = input("What is your name?\n")
+
+print('Great, your name is ' + name)
---
+++ week_1/print/print.py
@@ -0,0 +1,14 @@
+# the print keyword is used to display a message
+print('Hello world!')
+
+# Strings can be enclosed in single quotes
+print('Hello world single quotes')
+
+# Strings can also be enclosed in double quotes
+print("Hello world - double quotes")
+
+# using print to display integer / whole numbers
+print(188)
+
+# using print to display float number / real number
+print(99.9)
